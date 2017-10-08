0f7a044f111e7697e6304636491a08841763f1f6
Michael Navazhylau <mikipux7@gmail.com>
2017-10-08 14:29:13 -0500

    added ability to save logs

---
+++ src/db/AnimalLogDatabase.ts
@@ -0,0 +1,40 @@
+import localForage from "localforage";
+
+import { Log } from '../model/Log';
+
+export class AnimalLogDatabase {
+
+  static setAnimalLog(animal:string, animal_logs:Log[]) {
+
+    var set_animal_promise = new Promise(function (resolve, reject) {
+      localforage.setItem(animal, animal_logs).then (function () {
+        return localforage.getItem(animal);
+      }).then (function (value) {
+        resolve(value);
+      }).catch (function (err) {
+        reject(err);
+      })
+    })
+
+    return set_animal_promise;
+
+  }
+
+  static getAnimalLogs(animal:string) {
+
+    var get_animal_promise = new Promise (function (resolve, reject) {
+
+      localforage.getItem(animal).then (function (logs) {
+          resolve (logs)
+      }).catch (function (error) {
+          resolve([]);
+          console.log(error);
+      })
+
+    })
+
+    return get_animal_promise;
+
+  }
+
+}
