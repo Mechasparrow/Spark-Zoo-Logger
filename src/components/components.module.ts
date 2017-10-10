a088054f4b7b4fea21e2ed65b5484744d2d4af15
Michael Navazhylau <mikipux7@gmail.com>
2017-10-10 18:15:53 -0500

    rolled out skeleton code

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FeedingComponent } from './feeding/feeding';
 import { AnimalStatusComponent } from './animal-status/animal-status';
+import { AnimalObservationComponent } from './animal-observation/animal-observation';
 @NgModule({
 	declarations: [FeedingComponent,
-    AnimalStatusComponent],
+    AnimalStatusComponent,
+    AnimalObservationComponent],
 	imports: [],
 	exports: [FeedingComponent,
-    AnimalStatusComponent]
+    AnimalStatusComponent,
+    AnimalObservationComponent]
 })
 export class ComponentsModule {}
