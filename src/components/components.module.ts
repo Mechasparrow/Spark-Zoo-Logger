9b651c1c2546bcf3f7fdf9453568a156ffd93a19
Michael Navazhylau <mikipux7@gmail.com>
2017-10-10 18:27:10 -0500

    fixed bug

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FeedingComponent } from './feeding/feeding';
 import { AnimalStatusComponent } from './animal-status/animal-status';
-import { AnimalObservationComponent } from './animal-observation/animal-observation';
 import { AnimalBehaviorComponent } from './animal-behavior/animal-behavior';
 @NgModule({
 	declarations: [FeedingComponent,
     AnimalStatusComponent,
-    AnimalObservationComponent,
     AnimalBehaviorComponent],
 	imports: [],
 	exports: [FeedingComponent,
     AnimalStatusComponent,
-    AnimalObservationComponent,
     AnimalBehaviorComponent]
 })
 export class ComponentsModule {}
